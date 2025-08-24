5c56ec0c29e9c643d676268cdea33c93a6a66ac4
YuorBass <iamzvir777@gmail.com>
2025-08-24 20:30:56 +0300

    урок Обьекты

---
+++ master_of_code_v2/part_1/42. Объекты/main.js
@@ -0,0 +1,15 @@
+// 42. Объекты
+
+'use strict';
+
+// запись в виде массива. Массивы указываются внутри []
+const myData = ['YouRa', 'Alakhverdov', 1974, 'programming instructor', ['Irina', 'Michael']];
+
+// запись в виде обьекта. Обьекты указываются внутри {}
+const myData1 = {
+  name: 'Youra',
+  lastName: 'Alakhverdov',
+  birthYear: 1974,
+  job: 'programming instructor',
+  familyMembers: ['Irina', 'Michael']
+}
