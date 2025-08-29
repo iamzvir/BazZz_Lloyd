538dc856394a263b7b9c337ff0ba1a3a4f559820
YuorBass <iamzvir777@gmail.com>
2025-08-29 22:07:44 +0300

    Anrew_trolololo_Break & Continue

---
+++ master_of_code_v2/part_1/49. Break & Continue/main.js
@@ -0,0 +1,28 @@
+
+'use strict'
+
+// 49. Break & Continue
+
+const user123 = ['YouRa', 'Alkoholov', 1974, 'developer', ['Pidr', 'Loh'], false];
+
+// 1. continue - цикл работает пока в условии будет встречаться true
+
+for (let i = 0; i < user123.length; i++) {
+
+  // continue - перебирает весь массив, но выводит результат согласно указанному условию в if() 
+  if (typeof user123[i] !== 'string') continue;
+
+  console.log(user123[i], typeof user123[i]); // 'YouRa' string, 'Alkoholov' string, 'developer' string
+}
+
+console.log('\n');
+
+// 2. break - сразу выходит из цикла если в указанном условии в if() получается - true.
+
+for (let i = 0; i < user123.length; i++) {
+
+  // интерпретатор выводит элементы любых типов, а когда натыкается на число, выходит из цикла.
+  if (typeof user123[i] === 'number') break;
+
+  console.log(user123[i], typeof user123[i]); // 'YouRa' string, 'Alkoholov' string
+}
