c4329f8db2682cb27f69378446bd2b3f2a8ea41a
AndrewShand777 <shand2004@gmail.com>
2025-10-06 11:43:29 +0300

    123

--- master_of_code_v2/part_2/3. Необходимые Скиллы Разработчика/3.6. Поиск информации в интернете(разбор про амплитуду влажности)/clock.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('button');
-
-button.onclick = function () {
-  navigator.clipboard.writeText('Yura BazZz - красный глаз, из-за угла он смотрит на ВаzZz :)');
-}
